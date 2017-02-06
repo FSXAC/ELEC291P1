e629ef20b13fafde2827591dc2c8bfbab697fdb1
Muchen He <mansurhe@outlook.com>
2017-02-06 13:21:10 -0800

    Reorganized folders

--- __a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-F:
-cd "\OneDrive\Documents\2017 UBC\ELEC 291\Project 1\ELEC291P1\"
-"F:\OneDrive\Documents\2017 UBC\ELEC 291\CrossIDE\Call51\Bin\a51.exe" -l "F:\OneDrive\Documents\2017 UBC\ELEC 291\Project 1\ELEC291P1\interface.asm"
-echo Crosside_Action Set_Hex_File F:\OneDrive\Documents\2017 UBC\ELEC 291\Project 1\ELEC291P1\interface.HEX
