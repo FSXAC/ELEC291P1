b9779c3e7c9151510b87b077a84c7f0fcd8f545c
larrylikestocode <yunpengliu1997@126.com>
2017-02-12 16:30:09 -0800

    Regular update

--- src/interface.asm
+++ src/interface.asm
@@ -150,6 +150,8 @@ T0_ISR:
 ;---------------------------
 ; Configure the serial port and baud rate using timer 1
 InitSerialPort:
+    PUSH AR0
+    PUSH AR1
     ; Since the reset button bounces, we need to wait a bit before
     ; sending messages, or risk displaying gibberish!
     mov R1, #222
@@ -165,7 +167,61 @@ InitSerialPort:
     mov	TL1,#T1LOAD
     setb TR1
     mov	SCON,#0x52
+    
+    POP AR1
+    POP AR0
     ret
+    
+	
+; Send a character using the serial port
+putchar:
+    jnb TI, putchar
+    clr TI
+    mov SBUF, a
+    ret
+
+; Send a constant-zero-terminated string using the serial port
+SendString:
+    clr A
+    movc A, @A+DPTR
+    jz SendStringDone
+    lcall putchar
+    inc DPTR
+    sjmp SendString
+SendStringDone:
+    ret
+
+Hello_World:
+    DB  'Hello, World!', '\r', '\n', 0    
+    
+    
+;----------------------------------------------------------------------
+; These ?EQU? must match the wiring between the microcontroller and ADC
+;----------------------------------------------------------------------
+
+INIT_SPI:
+ 	setb MY_MISO ; Make MISO an input pin
+ 	clr MY_SCLK ; For mode (0,0) SCLK is zero
+ 	ret
+
+DO_SPI_G:
+ 	push acc
+ 	mov R1, #0 ; Received byte stored in R1
+ 	mov R2, #8 ; Loop counter (8-bits)
+DO_SPI_G_LOOP:
+ 	mov a, R0 ; Byte to write is in R0
+ 	rlc a ; Carry flag has bit to write
+ 	mov R0, a
+ 	mov MY_MOSI, c
+ 	setb MY_SCLK ; Transmit
+ 	mov c, MY_MISO ; Read received bit
+ 	mov a, R1 ; Save received bit in R1
+ 	rlc a
+    mov R1, a
+ 	clr MY_SCLK
+ 	djnz R2, DO_SPI_G_LOOP
+ 	pop acc
+ 	ret
     
 
 ; -------------------------;
